bf97efc977436d9210e1db31385a319aa45ff6d4
Heluuuu <holaaaa@Helooo-2.local>
2025-10-27 22:21:26 -0600

    git test

--- spread_diff.py
+++ spread_diff.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import yfinance as yf
 csv_path = "data/AAPL_MSFT_aligned.csv"
 table = pd.read_csv(csv_path, parse_dates=["Date"], index_col="Date")
 table["Spread_Diff"] = table["AAPL"]-table["MSFT"]
 table.to_csv("data/AAPL_MSFT_spread.csv", index=False)
 table["Spread_Diff"].mean()
 table["Spread_Diff"].std()
+print("Hello world")
 
 # print("First 5 rows")
 # print(table.head())
